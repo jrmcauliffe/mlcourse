d19d37232d8452e2d6c3b7eda96342e652e084c3
Jonathan McAuliffe <jon@birchavenue.org>
2018-08-19 09:32:19 +1000

    Add predictor and reformat

--- ex3/predictOneVsAll.m
+++ ex3/predictOneVsAll.m
@@ -13,6 +13,7 @@ num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
+pval = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -30,13 +31,8 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
-
-
-
-
-
+[pval, p] =  max(sigmoid(X * all_theta'), [], 2);
 
 % =========================================================================
-
 
 end
